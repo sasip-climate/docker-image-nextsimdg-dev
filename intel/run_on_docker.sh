a4c157af185879176732032e1eeefd249b20a493
Aurélie Albert <auraoupa@gmail.com>
2024-07-23 12:04:33 +0200

    Update run_on_docker.sh

--- intel/run_on_docker.sh
+++ intel/run_on_docker.sh
@@ -3,5 +3,5 @@ git clone -b develop https://github.com/nextsimhub/nextsimdg.git
 cd nextsimdg
 . /opt/spack-environment/activate.sh
 mkdir -p build && cd build
-cmake ..
+cmake .. -DENABLE_MPI=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$(which mpiicc) -DCMAKE_CXX_COMPILER=$(which mpiicpc)
 
